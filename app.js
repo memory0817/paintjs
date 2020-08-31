8771fb3e82a7034bb5dae820f0af76932eb88860
memory <memory140720@gmail.com>
2020-08-31 23:26:20 +0900

    200831 작업현황

---
+++ app.js
@@ -0,0 +1,47 @@
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
+
+let painting = false;
+
+function stopPainting() {
+    painting = false;
+}
+
+function startPainting() {
+    painting = true;
+}
+
+function onMouseMove(event) {
+    const x = event.offsetX;
+    const y = event.offsetY;
+    //console.log(x,y);
+    if(!painting) {
+        ctx.beginPath();
+        ctx.MoveTo(x, y);
+    }else {
+        ctx.lineTo(x,y);
+        ctx.stroke();
+    }
+}
+
+function onMouseDown(event) {
+    painting = true;
+}
+
+// function onMouseUp(event) {
+//     stopPainting();
+// }
+
+// function onMouseLeave(event) {
+//     stopPainting();
+// }
+
+if(canvas){
+    canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
+    canvas.addEventListener("mouseleave",stopPainting);
+}
