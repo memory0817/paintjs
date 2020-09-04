81b2673027ef483c46f4fdd1ad582ebc09eaf27a
memory <memory140720@gmail.com>
2020-09-05 05:39:33 +0900

    2020.09.04 변경된사항 커밋

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
 
-canvas.width = 700;
-canvas.height = 700;
+const INITIAL_COLOR = "#2c2c2c"; //언제 이런 변수를 만드냐면 반복되는 행위가 생기기 시작할때(라고 니콜라스는 스스로약속했다고함)
+const CANVAS_SIZE = 700;
 
-ctx.strokeStyle = "#2c2c2c";
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
+
+
 
 function stopPainting() {
     painting = false;
@@ -23,16 +34,13 @@ function onMouseMove(event) {
     //console.log(x,y);
     if(!painting) {
         ctx.beginPath();
-        ctx.MoveTo(x, y);
+        ctx.moveTo(x, y);
     }else {
         ctx.lineTo(x,y);
         ctx.stroke();
     }
 }
 
-function onMouseDown(event) {
-    painting = true;
-}
 
 // function onMouseUp(event) {
 //     stopPainting();
@@ -42,9 +50,70 @@ function onMouseDown(event) {
 //     stopPainting();
 // }
 
+function handleColorClick(event){
+    //console.log(event.target.style);
+    const color = event.target.style.backgroundColor;
+    //console.log(color);
+    ctx.strokeStyle = color;
+    ctx.fillStyle = ctx.strokeStyle;
+}
+
+
+function handleRangeChange(event) {
+    const size = event.target.value;
+    ctx.lineWidth = size;
+}
+
+function handleModeClick() {
+    if(filling === true) {
+        filling = false;
+        mode.innerText = "Fill";
+    } else {
+        filling = true;
+        mode.innerText = "Paint";
+    }
+}
+
+
+function handleCanvasClick() {
+    if(filling) {
+        ctx.fillRect(0,0,CANVAS_SIZE,CANVAS_SIZE);
+    }
+}
+
+
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave",stopPainting);
+    canvas.addEventListener("click", handleCanvasClick);
 }
+
+
+
+
+
+Array.from(colors).forEach(color =>
+    color.addEventListener("click",handleColorClick)
+);
+
+
+
+if(range) {
+    range.addEventListener("input", handleRangeChange);
+};
+
+
+if(mode){
+    mode.addEventListener("click", handleModeClick);
+}
+
+
+/*
+Array.from(colors).forEach(potato =>
+    potato.addEventListener("click",handleColorClick)
+);
+potato로 해도된다. 상관없다. 그냥 그 array 안에 있는 각각의 아이템들을 대표하는 것뿐이다. 각각의 div일 뿐이다.
+*/
+
