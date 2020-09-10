1f635fd6e19bc0a44fc46ddae6fcad6c3bc44271
memory <memory140720@gmail.com>
2020-09-11 04:51:55 +0900

    2020.09.10 9월10일 작업물 커밋

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
 
 const INITIAL_COLOR = "#2c2c2c"; //언제 이런 변수를 만드냐면 반복되는 행위가 생기기 시작할때(라고 니콜라스는 스스로약속했다고함)
 const CANVAS_SIZE = 700;
@@ -10,7 +11,8 @@ const CANVAS_SIZE = 700;
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
 
-
+ctx.fillStyle = "white";
+ctx.fillRect(0,0,CANVAS_SIZE,CANVAS_SIZE);
 ctx.strokeStyle = INITIAL_COLOR;
 ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
@@ -81,6 +83,18 @@ function handleCanvasClick() {
     }
 }
 
+function handleCM(event) {
+    event.preventDefault();
+}
+
+function handleSaveClick(){
+    const image = canvas.toDataURL(); //"image/png" 확장자명을 바꾸면 타입이 바뀐다. 기본은 png
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "PaintJS[🎨]";
+    //console.log(link);
+    link.click();
+}
 
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
@@ -88,6 +102,7 @@ if(canvas){
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave",stopPainting);
     canvas.addEventListener("click", handleCanvasClick);
+    canvas.addEventListener("contextmenu", handleCM);
 }
 
 
@@ -107,6 +122,11 @@ if(range) {
 
 if(mode){
     mode.addEventListener("click", handleModeClick);
+}
+
+
+if(saveBtn) {
+    saveBtn.addEventListener("click", handleSaveClick);
 }
 
 
